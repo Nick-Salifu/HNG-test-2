26cc1e4e27cf9562f2be18f14ed74703ceb249d4
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-07-11 15:22:26 +0100

    reducing gap on new arrival

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -11,8 +11,8 @@ function NewArrivals() {
                 </div>
 
                 <div className="w-[80%] left-[10%] right-[10%] lg:w-[60%] rounded-2xl lg:rounded-lg bg-[#f1f1f1]/60 absolute top-[12%] lg:right-0 lg:left-64 lg:top-[25%]">
-                    <div className="flex flex-col items-center justify-center gap-2 lg:gap-8 py-5 lg:py-0">
-                        <h1 className=" max-w-sm text-2xl text-center capitalize text-white opacity-100 font-days z-50 lg:mt-[80px]">High quality print T-Shirt now available</h1>
+                    <div className="flex flex-col items-center justify-center gap-2 lg:gap-8 py-3 lg:py-0">
+                        <h1 className=" max-w-sm text-2xl text-center capitalize text-white opacity-100 font-days lg:mt-[80px]">High quality print T-Shirt now available</h1>
                         <button className="px-7 py-1 text-white bg-black lg:mb-[50px] rounded-lg">Shop</button>
                     </div>
                 </div>
