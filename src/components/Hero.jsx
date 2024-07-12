30da8cf1834bd1aaa608c1d77549bd5c3852281b
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-07-12 14:55:48 +0100

    reducing the tracking

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
 
                 <div className="relative">
                     <div className="absolute flex flex-col items-center text-white lg:items-start top-[30%] w-full mx-auto lg:top-44  lg:mx-20">
-                        <h1 className="mb-4 text-3xl tracking-wider uppercase lg:text-7xl font-days">Pandas clothing</h1>
+                        <h1 className="mb-4 text-3xl tracking-wide uppercase md:tracking-wider lg:text-7xl font-days">Pandas clothing</h1>
                         <ul className="space-y-6 text-sm capitalize font-days">
                             <li className="flex items-center gap-1"><span><img src="/images/whitearrow.png" alt="" /></span>shoes</li>
                             <li className="ml-2">accessories</li>
